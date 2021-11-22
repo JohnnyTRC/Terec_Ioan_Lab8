afc461e69270f247cbf871cbf0d705068040acbe
Ioan-Samuel Terec <terec_ioan@yahoo.com>
2021-11-22 13:03:53 +0200

    Add project files.

---
+++ Data/Terec_Ioan_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Terec_Ioan_Lab8.Models;
+
+namespace Terec_Ioan_Lab8.Data
+{
+    public class Terec_Ioan_Lab8Context : DbContext
+    {
+        public Terec_Ioan_Lab8Context (DbContextOptions<Terec_Ioan_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Terec_Ioan_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Terec_Ioan_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Terec_Ioan_Lab8.Models.Category> Category { get; set; }
+    }
+}
